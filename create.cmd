9d7114537bbfcbaf9f1409eae351f428b7cf3ad6
Tobias <tobias.hajszan@outlook.com>
2020-04-20 14:58:14 +0200

    changed workflow

--- create.cmd
+++ create.cmd
@@ -1,18 +1,13 @@
 @echo off
-echo Generating missing nfo and image files for you
+echo Generating missing image files for you
 
 for %%f in (*.fst) do (
     for /F "tokens=1,3 delims=." %%a in ("%%f") do (
-       if not exist %%a.nfo (
-        echo Missing nfo file for %%a
-        echo Bitmap=%%a.png> %%a.nfo
-        attrib +h "%%a.nfo"
-       )
        if not exist %%a.png (
          echo Missing png file for %%a
          echo > %%a.png
        )
     )
 )
-echo created all missing files.
+echo created all missing images for your plugins. Now save the screenshots into the files!
 pause
--- thumbnail.cmd
+++ thumbnail.cmd
@@ -5,7 +5,12 @@ for %%f in (*.fst) do (
     for /F "tokens=1,3 delims=." %%a in ("%%f") do (
        python thumbnailer.py "%%a.png"
        attrib +h "%%a.png"
+       if not exist %%a.nfo (
+        echo Creating nfo file linking to %%a.png
+        echo Bitmap=%%a.png> %%a.nfo
+        attrib +h "%%a.nfo"
+       )
     )
 )
-echo Done. All images should now have small size!
+echo Done. All images should now have small size and nfos linking to them, take a look!
 pause
