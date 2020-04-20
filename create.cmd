5aad18be1bcb6d49f1607e9f3e78d77a16a56240
Tobias <tobias.hajszan@outlook.com>
2020-04-20 14:31:31 +0200

    removed whitespace from .nfo files, hidden attribute is done after thumbnailing

--- create.cmd
+++ create.cmd
@@ -5,13 +5,12 @@ for %%f in (*.fst) do (
     for /F "tokens=1,3 delims=." %%a in ("%%f") do (
        if not exist %%a.nfo (
         echo Missing nfo file for %%a
-        echo Bitmap = %%a.png > %%a.nfo
+        echo Bitmap=%%a.png> %%a.nfo
         attrib +h "%%a.nfo"
        )
        if not exist %%a.png (
          echo Missing png file for %%a
          echo > %%a.png
-         attrib +h "%%a.png"
        )
     )
 )
--- thumbnail.cmd
+++ thumbnail.cmd
@@ -4,7 +4,8 @@ echo Thumbnailing pictures for you
 for %%f in (*.fst) do (
     for /F "tokens=1,3 delims=." %%a in ("%%f") do (
        python thumbnailer.py "%%a.png"
+       attrib +h "%%a.png"
     )
 )
-echo Done. All Images should now have small size!
+echo Done. All images should now have small size!
 pause
